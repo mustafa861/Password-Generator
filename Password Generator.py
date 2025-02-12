7b0b309ca957e2273186d2d7123dd18730dfe478
mustafa861 <mustafa1212adnan@gmail.com>
2025-02-12 15:10:53 +0500

    Create Password Generator.py

---
+++ Password Generator.py
@@ -0,0 +1,16 @@
+import random
+
+print("Welcome To Your Password Generator")
+
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*().,?0123456789"
+
+number = int(input("Amount of passwords to generate: "))
+length = int(input("Input your password length: "))
+
+print("\nHere are your passwords:")
+
+for pwd in range(number):
+    password = ""
+    for c in range(length):
+        password += random.choice(chars)  # Corrected variable name
+    print(password)
